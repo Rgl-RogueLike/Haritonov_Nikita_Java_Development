7935122a43c25aca25ccae13744ef505e4b0439f
Nikita Haritonov <nikita.harit@mail.ru>
2026-02-03 14:01:54 +0300

    Added "CalculationConstants"

---
+++ src/main/java/com/haritonov/vacation_pay/util/CalculationConstants.java
@@ -0,0 +1,11 @@
+package com.haritonov.vacation_pay.util;
+
+import java.math.BigDecimal;
+
+public class CalculationConstants {
+
+    public static final BigDecimal AVG_DAYS_IN_MONTH = new BigDecimal("29.3");
+
+    private CalculationConstants() {}
+
+}
