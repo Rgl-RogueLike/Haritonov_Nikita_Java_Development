41fd491d7ccac6095dd705a7f064ba33cfb288a4
Nikita Haritonov <nikita.harit@mail.ru>
2026-02-03 15:31:23 +0300

    added GET /calculate endpoint

--- src/main/java/com/haritonov/vacation_pay/controller/VacationPayController.java
+++ src/main/java/com/haritonov/vacation_pay/controller/VacationPayController.java
@@ -1,4 +1,28 @@
 package com.haritonov.vacation_pay.controller;
 
+import com.haritonov.vacation_pay.dto.VacationPayRequest;
+import com.haritonov.vacation_pay.dto.VacationPayResponse;
+import com.haritonov.vacation_pay.service.VacationPayService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@Validated
 public class VacationPayController {
+
+    private final VacationPayService vacationPayService;
+
+    public VacationPayController(VacationPayService vacationPayService) {
+        this.vacationPayService = vacationPayService;
+    }
+
+    @GetMapping("/calculate")
+    public ResponseEntity<VacationPayResponse> calculate(@Valid VacationPayRequest request) {
+        VacationPayResponse response = vacationPayService.calculate(request);
+        return ResponseEntity.ok(response);
+    }
 }
