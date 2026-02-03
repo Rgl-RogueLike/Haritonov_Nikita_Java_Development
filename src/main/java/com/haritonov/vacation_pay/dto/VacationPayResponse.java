783e4e0a88ac7b149bab816d6a556e1f7bba6192
Nikita Haritonov <nikita.harit@mail.ru>
2026-02-03 14:19:39 +0300

    Added fields, constructors, getters and setters

--- src/main/java/com/haritonov/vacation_pay/dto/VacationPayResponse.java
+++ src/main/java/com/haritonov/vacation_pay/dto/VacationPayResponse.java
@@ -1,4 +1,22 @@
 package com.haritonov.vacation_pay.dto;
 
+import java.math.BigDecimal;
+
 public class VacationPayResponse {
+
+    private BigDecimal calculationResult;
+
+    public VacationPayResponse() {}
+
+    public VacationPayResponse(BigDecimal calculationResult) {
+        this.calculationResult = calculationResult;
+    }
+
+    public BigDecimal getCalculactionResult() {
+        return calculationResult;
+    }
+
+    public void setCalculactionResult(BigDecimal calculationResult) {
+        this.calculationResult = calculationResult;
+    }
 }
