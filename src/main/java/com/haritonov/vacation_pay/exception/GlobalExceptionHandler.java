fcf576cbbbbd74e092f21bdd12e57e32eec0a893
Nikita Haritonov <nikita.harit@mail.ru>
2026-02-03 14:55:57 +0300

    Added error handling: data validation, logic, and server

--- src/main/java/com/haritonov/vacation_pay/exception/GlobalExceptionHandler.java
+++ src/main/java/com/haritonov/vacation_pay/exception/GlobalExceptionHandler.java
@@ -1,4 +1,36 @@
 package com.haritonov.vacation_pay.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        FieldError error = exception.getBindingResult().getFieldError();
+        String errorMessage = (error != null) ? error.getDefaultMessage() : "Ошибка валидации данных";
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(errorMessage));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneralException(Exception exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse("Внутрення ошибка сервера: " + exception.getMessage()));
+    }
+
 }
