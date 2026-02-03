700c4155c99369632d8a8c6e858e7cb86f4970b6
Nikita Haritonov <nikita.harit@mail.ru>
2026-02-03 17:59:34 +0300

    Added logic for calculating vacation pay based on the start and end dates

--- src/main/java/com/haritonov/vacation_pay/service/strategy/DateRangePayCalculator.java
+++ src/main/java/com/haritonov/vacation_pay/service/strategy/DateRangePayCalculator.java
@@ -1,12 +1,45 @@
 package com.haritonov.vacation_pay.service.strategy;
 
+import com.haritonov.vacation_pay.config.HolidaysProperties;
 import com.haritonov.vacation_pay.dto.VacationPayRequest;
+import com.haritonov.vacation_pay.util.CalculationConstants;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
+@Component
 public class DateRangePayCalculator implements VacationPayStrategy {
+
+    private final HolidaysProperties holidaysProperties;
+
+    public DateRangePayCalculator(HolidaysProperties holidaysProperties) {
+        this.holidaysProperties = holidaysProperties;
+    }
+
     @Override
     public BigDecimal calculate(VacationPayRequest request) {
-        return null;
+        LocalDate startDate = request.getStartDate();
+        LocalDate endDate = request.getEndDate();
+        if (startDate == null || endDate == null) {
+            throw new IllegalArgumentException("Необходимы даты начала и конца отпуска");
+        }
+        long totalDays = ChronoUnit.DAYS.between(startDate, endDate) + 1;
+        int paidDays = 0;
+        for (int i = 0; i < totalDays; i++) {
+            LocalDate currentDate = startDate.plusDays(i);
+            if (!holidaysProperties.isHolidays(currentDate)) {
+                paidDays++;
+            }
+        }
+        if (paidDays == 0) {
+            return  BigDecimal.ZERO;
+        }
+        BigDecimal averageSalary = request.getAverageSalary();
+        BigDecimal averageDailySalary = averageSalary.divide(CalculationConstants.AVG_DAYS_IN_MONTH, 2, RoundingMode.HALF_UP);
+
+        return averageDailySalary.multiply(BigDecimal.valueOf(paidDays));
     }
 }
