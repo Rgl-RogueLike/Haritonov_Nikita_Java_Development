d294dae8d2b422bfbea3d71255fcd44908a2b82a
Nikita Haritonov <nikita.harit@mail.ru>
2026-02-03 14:33:40 +0300

    Added a method "calculate" in VacationPayService

--- src/main/java/com/haritonov/vacation_pay/service/VacationPayService.java
+++ src/main/java/com/haritonov/vacation_pay/service/VacationPayService.java
@@ -1,4 +1,10 @@
 package com.haritonov.vacation_pay.service;
 
+import com.haritonov.vacation_pay.dto.VacationPayRequest;
+import com.haritonov.vacation_pay.dto.VacationPayResponse;
+
 public interface VacationPayService {
+
+    VacationPayResponse calculate(VacationPayRequest request);
+
 }
